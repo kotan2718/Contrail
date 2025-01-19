3f20bd746fba872ffa2cfc1ef3c372035ac58c27
kazuo <vff06643@nifty.ne.jp>
2025-01-19 16:32:27 +0900

    Make updates to multiple files

--- libs/etc/js/test/libs/jQuery_/kz/js/jq_kz_fade.js
+++
@@ -1,9 +0,0 @@
-charset="UTF8"
-
-/* picture change with fade */
-$(function(){
-	$('.pic_change_fade img').toggle(
-		function(){$(this).fadeTo(600, 0);},
-		function(){$(this).fadeTo(200, 1.0);}
-	);
-});
--- libs/etc/js/test/libs/jQuery_/kz/js/jq_kz_index_welcome.js
+++
@@ -1,9 +0,0 @@
-charset="UTF8"
-
-	$(window).load(function(){
-		var delaySpeed = 3000;
-		var fadeSpeed = 5000;
-		$('.welcome img').each(function(i){
-			$(this).delay(i*(delaySpeed)).css({display:'block',opacity:'0'}).animate({opacity:'1'},fadeSpeed);
-		});
-	});
